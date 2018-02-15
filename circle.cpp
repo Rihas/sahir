9f0391c945dc81938db04ee335e2e293af6df190
Sajad allithy <sajad.allithy@gmail.com>
2018-02-15 11:41:37 -0600

    this is the firs commit

---
+++ circle.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : circle.cpp
+// Author      : sahir
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#define pi 3.1428571429
+#include <math.h>
+
+
+using namespace std;
+
+
+
+int main() {
+	double r , area;
+
+	cout << "Input the radius of circle please \n"; cin>> r ;
+	area=pi*pow(r,2);
+	cout << "Area equals " << area << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
